4ab85b5c0b0f68ecb65cfb5eb02ac70a61304ac3
Ethan Woodbury <woodburyethan60@gmail.com>
2025-07-31 00:39:07 -0700

    Make tests more focused, new Shuffle tests

--- src/test/scala/com/ewoodbury/sparklet/core/TestShuffleOperationsPlan.scala
+++ src/test/scala/com/ewoodbury/sparklet/core/TestShuffleOperationsPlan.scala
@@ -3,7 +3,7 @@ package com.ewoodbury.sparklet.core
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class TestShuffleOperations extends AnyFlatSpec with Matchers {
+class TestShuffleOperationsPlan extends AnyFlatSpec with Matchers {
   val toDistCollection = [T] => (seq: Seq[T]) => DistCollection(Plan.Source(Seq(Partition(seq))))
 
   "Shuffle transformations" should "create groupByKey transformations without triggering computation" in {
@@ -12,6 +12,12 @@ class TestShuffleOperations extends AnyFlatSpec with Matchers {
     
     // Should create the transformation successfully
     grouped.plan shouldBe a[Plan.GroupByKeyOp[_, _]]
+    
+    // Verify the source plan is preserved
+    grouped.plan match {
+      case Plan.GroupByKeyOp(sourcePlan) => sourcePlan shouldBe source.plan
+      case _ => fail("Expected GroupByKeyOp")
+    }
   }
 
   it should "create reduceByKey transformations without triggering computation" in {
@@ -20,6 +26,12 @@ class TestShuffleOperations extends AnyFlatSpec with Matchers {
     
     // Should create the transformation successfully  
     reduced.plan shouldBe a[Plan.ReduceByKeyOp[_, _]]
+    
+    // Verify the source plan is preserved
+    reduced.plan match {
+      case Plan.ReduceByKeyOp(sourcePlan, _) => sourcePlan shouldBe source.plan
+      case _ => fail("Expected ReduceByKeyOp")
+    }
   }
 
   it should "create sortBy transformations without triggering computation" in {
@@ -28,6 +40,12 @@ class TestShuffleOperations extends AnyFlatSpec with Matchers {
     
     // Should create the transformation successfully
     sorted.plan shouldBe a[Plan.SortByOp[_, _]]
+    
+    // Verify the source plan is preserved
+    sorted.plan match {
+      case Plan.SortByOp(sourcePlan, _, _) => sourcePlan shouldBe source.plan
+      case _ => fail("Expected SortByOp")
+    }
   }
 
   it should "create join transformations without triggering computation" in {
@@ -37,6 +55,14 @@ class TestShuffleOperations extends AnyFlatSpec with Matchers {
     
     // Should create the transformation successfully
     joined.plan shouldBe a[Plan.JoinOp[_, _, _]]
+    
+    // Verify both source plans are preserved
+    joined.plan match {
+      case Plan.JoinOp(leftPlan, rightPlan) => 
+        leftPlan shouldBe left.plan
+        rightPlan shouldBe right.plan
+      case _ => fail("Expected JoinOp")
+    }
   }
 
   it should "create cogroup transformations without triggering computation" in {
@@ -46,56 +72,45 @@ class TestShuffleOperations extends AnyFlatSpec with Matchers {
     
     // Should create the transformation successfully
     cogrouped.plan shouldBe a[Plan.CoGroupOp[_, _, _]]
+    
+    // Verify both source plans are preserved
+    cogrouped.plan match {
+      case Plan.CoGroupOp(leftPlan, rightPlan) => 
+        leftPlan shouldBe left.plan
+        rightPlan shouldBe right.plan
+      case _ => fail("Expected CoGroupOp")
+    }
   }
 
-  it should "execute groupByKey operations using DAG scheduler" in {
+  it should "create chained narrow-then-shuffle transformation plans correctly" in {
+    val source = toDistCollection(Seq(1 -> "one", 2 -> "two"))
+    val mapped = source.map(x => (x._1 + 10, x._2.toUpperCase(java.util.Locale.ENGLISH)))
+    val grouped = mapped.groupByKey
+    
+    // Should create the transformation chain correctly
+    grouped.plan shouldBe a[Plan.GroupByKeyOp[_, _]]
+    
+    // Verify the structure
+    grouped.plan match {
+      case Plan.GroupByKeyOp(innerPlan) => 
+        innerPlan shouldBe a[Plan.MapOp[_, _]]
+      case _ => fail("Expected GroupByKeyOp")
+    }
+  }
+
+  it should "create chained shuffle-then-narrow transformation plans correctly" in {
     val source = toDistCollection(Seq(1 -> "one", 2 -> "two"))
     val grouped = source.groupByKey
+    val mapped = grouped.map(_.toString)
     
-    val expected = Map(1 -> Seq("one"), 2 -> Seq("two"))
+    // Should create the transformation chain correctly
+    mapped.plan shouldBe a[Plan.MapOp[_, _]]
     
-    val result = grouped.collect()
-
-    val sortedResult = result.toSeq.sortBy(_._1)
-    val sortedExpected = expected.toSeq.sortBy(_._1)
-    sortedResult shouldBe sortedExpected
-  }
-
-  it should "execute reduceByKey operations using DAG scheduler" in {
-    val source = toDistCollection(Seq("one" -> 1, "one" -> 1, "two" -> 2))
-    val reduced = source.reduceByKey((a: Int, b: Int) => a + b)
-    
-    val expected = Map("one" -> 2, "two" -> 2)
-    
-    val result = reduced.collect()
-
-    val sortedResult = result.toSeq.sortBy(_._1)
-    val sortedExpected = expected.toSeq.sortBy(_._1)
-    sortedResult shouldBe sortedExpected
-  }
-
-  it should "execute sortBy operations using DAG scheduler" in {
-    val source = toDistCollection(Seq(3, 1, 4, 1, 5))
-    val sorted = source.sortBy(identity)
-    
-    val expected = Seq(1, 1, 3, 4, 5)
-    
-    val result = sorted.collect()
-    result shouldBe expected
-  }
-
-  // TODO: Implement this
-  ignore should "execute chained narrow and wide transformations using DAG scheduler" in {
-    val source = toDistCollection(Seq(1 -> "one", 2 -> "two"))
-    val chained = source.groupByKey.map(_.toString)
-    
-    // Should create the chained transformation successfully
-    chained.plan shouldBe a[Plan.MapOp[_, _]]
-    
-    // Should execute successfully with DAG scheduler
-    noException should be thrownBy {
-      val result = chained.collect()
-      println(s"Chained transformations result: $result")
+    // Verify the structure
+    mapped.plan match {
+      case Plan.MapOp(innerPlan, _) => 
+        innerPlan shouldBe a[Plan.GroupByKeyOp[_, _]]
+      case _ => fail("Expected MapOp")
     }
   }
 }
